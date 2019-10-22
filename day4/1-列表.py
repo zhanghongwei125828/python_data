23d04c7b7b215efdd6b4b9236a3a31eee452048c
zhanghongwei125828 <15210032090@163.com>
2019-10-22 18:56:35 +0800

    公司 2019/10/21同步

---
+++ day4/1-列表.py
@@ -0,0 +1,25 @@
+# 列表操作方法
+# 取出列表数据,方法与str索引,切片一致
+'''
+a = ['zhangsan','lisi','wangwu','maliu']
+l1 = a[0]
+l2 = a[0:2]
+print(l1)
+print(l2)
+'''
+
+# 列表的增删改查
+
+# 增加 append  # 增加是增加到列表里面
+list1 = ['zhangsan','lisi','wangwu','maliu']
+l1 = list1.append('nick')
+print(list1)
+
+
+
+
+
+
+
+
+
