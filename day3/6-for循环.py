693d05dec62d82c45e36df2931a3b4d3bb1a3cb0
zhanghongwei125828 <15210032090@163.com>
2019-12-04 18:21:14 +0800

    公司 2019/12/4同步

--- day3/6-for循环.py
+++ day3/6-for循环.py
@@ -4,3 +4,10 @@ s1 = 'ABCDEFG'
 for i in s1:
     print(i)
 
+#ab12cd34ef56        # 找到字符串中有几对整数
+info = input('>>>')
+for x in info:
+    if x.isalpha():
+        info = info.replace(x,' ')
+l = info.split()
+print(len(l))
---
+++ day5/2-字典嵌套.py
@@ -0,0 +1,13 @@
+dict1 = {
+    'name':["zhangsan","lisi"],
+    'age' :{'a':18}
+}
+# dict1['name'][1] = dict1['name'][1].upper()       # 将字典中lisi改成大写
+# print(dict1)
+
+# dict1['age']['b'] =  19                             # 在字典dict1里面的age字典里面新增键值对
+# print(dict1)
+
+
+
+
